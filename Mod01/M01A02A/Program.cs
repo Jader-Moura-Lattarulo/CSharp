61b1aab687b799e0668b162e510739401e3ab5c3
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-01-06 17:15:24 -0300

    Add entradas e saídas

---
+++ Mod01/M01A02A/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace M01A02A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oi");
+            Console.Write("Tudo bem?");
+            Console.ReadKey();
+        }
+    }
+}
