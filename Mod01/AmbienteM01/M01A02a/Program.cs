acaaa61ef30be5fca78d0ef763af49d4be5a1147
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-01-06 17:57:47 -0300

    Add tipos de entrada

---
+++ Mod01/AmbienteM01/M01A02a/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace M01A02a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oi");
+            Console.WriteLine("Tudo bem?");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Mod01/AmbienteM01/M01A02c/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace M01A02c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jad\ter");
+            Console.WriteLine("Jad\ber");
+            Console.WriteLine("Jader\a");
+            Console.WriteLine("Jad\rer");
+            Console.WriteLine("\\Jader\\");
+            Console.WriteLine(@"\Jader\");
+            Console.WriteLine("\"Jader\"");
+            Console.ReadKey();
+
+            Console.WriteLine("C# é \n\"Super\" \nFácil!\a");
+        }
+    }
+}
