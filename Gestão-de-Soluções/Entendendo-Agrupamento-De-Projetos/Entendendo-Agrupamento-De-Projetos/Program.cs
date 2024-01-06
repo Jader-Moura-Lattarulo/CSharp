c1cfc66eaf1cd9adb010bee4d152c2a218d7db64
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-01-06 15:53:46 -0300

    Gestão de Soluções
    Aprendendo a fazer gestão de soluções no VS

---
+++ Gestão-de-Soluções/Entendendo-Agrupamento-De-Projetos/Entendendo-Agrupamento-De-Projetos/Program.cs
@@ -0,0 +1,19 @@
+﻿global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.IO;
+global using global::System.Linq;
+global using global::System.Net.Http;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
+
+namespace Teste1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Teste 1");
+            Console.ReadKey();
+        }
+    }
+}
