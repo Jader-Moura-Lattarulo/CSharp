a96879fed0d559e27779e98bc214acc9ad6e15a2
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-01-08 12:48:40 -0300

    Add comentários

---
+++ Mod01/AmbienteM01/M01A03/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace M01A03
+{
+    class Program
+    {
+        /// <summary>
+        /// Método executado automaticamente ao apertar Iniciar
+        /// ou executar diretamente pela linha de comando
+        /// </summary>
+        /// <param name="args">É um vetor que pode conter parâmetros de execução do meu programa</param>
+        /// <returns> Esse método não efetua retorno</returns>
+        /// <example>
+        /// <code>
+        ///     Podemos executar como Program.exe... (colocando os parâmetros)
+        /// </code>
+        /// </example>
+        static void Main(string[] args) 
+        {
+            // Comentário de uma única linha
+            Console.WriteLine("Olá Mundo!"); //Me livrei da maldição
+            
+            /*
+             * É um comentário de várias linhas
+             * Várias mesmo...
+             * Ele pode por um * em cada linha comentada
+             * Isso serve para quando precisa ser mais detalhado.
+             */
+
+
+        }
+    }
+}
