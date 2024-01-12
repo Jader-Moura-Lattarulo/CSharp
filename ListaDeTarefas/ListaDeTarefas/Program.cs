ee282eb2ee4a053a6fa1e07e4e1b2e2c86f79db8
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-01-12 20:55:14 -0300

    Add em CSharp 1 projeto
    Projeto de lista de tarefas um em português e outro em inglês.

---
+++ ListaDeTarefas/ListaDeTarefas/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Olá!");
+Console.WriteLine("O que você quer fazer?");
+Console.WriteLine("[V]er todas tarefas");
+Console.WriteLine("[A]dd uma tarefa");
+Console.WriteLine("[R]emover uma tarefa");
+Console.WriteLine("[S]air");
+
+Console.ReadKey();
