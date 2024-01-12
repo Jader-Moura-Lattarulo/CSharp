30f8ea8a9ce885bda52df257252270deed12e951
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-01-12 13:22:03 -0300

    Módulo 01 finalizado

--- Mod01/AmbienteM01/M01Ex009/Program.cs
+++ Mod01/AmbienteM01/M01Ex009/Program.cs
@@ -10,24 +10,28 @@ namespace M01Ex009
     {
         static void Main(string[] args)
         {
+            // Lendo Produto 01
             Console.Write("Produto 01: ");
-            String prod1 = Console.ReadLine();
+            String prod1 = Console.ReadLine().Trim();//".Trim()" -> Elimina os espaços no final da digitação
             Console.Write("Preço 01: R$");
-            int prç01;
-            int.TryParse(Console.ReadLine(), out prç01);
+            float prç01;
+            float.TryParse(Console.ReadLine().Trim(), out prç01);
 
+            // Lendo Produto 02
             Console.Write("Produto 02: ");
             String prod2 = Console.ReadLine();
             Console.Write("Preço 02: R$");
-            int prç02;
-            int.TryParse(Console.ReadLine(), out prç02);
+            float prç02;
+            float.TryParse(Console.ReadLine(), out prç02);
 
+            Console.SetCursorPosition(0,8);
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Blue;
-            Console.WriteLine(" Produto                    Preço ");
+            Console.WriteLine($"{"Produto", -20}{"Preço",13}");
             Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.Gray;
-            Console.WriteLine($"{prod1}");
+            Console.WriteLine($"{prod1,-20}{prç01,13:C}");
+            Console.WriteLine($"{prod2,-20}{prç02,13:C}");
 
 
             Console.ReadKey();
