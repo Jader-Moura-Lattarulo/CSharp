49004f8c1ab698ecbb0d3ed6ef173061a529fd48
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-01-13 14:39:16 -0300

    Calculator-Calculadora
    Exercício com uma calculadora simples onde o usuário digita dois números e escolhe em uma lista a ação desejada.
    
    Exercise with a simple calculator where the user enters two numbers and chooses the desired action from a list.

---
+++ Calculator-Calculadora/CalculadoraSimples/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Olá!");
+
+int primeiroNúmero; int segundoNúmero;
+
+Console.Write("Por favor insira o primeiro número: ");
+string entradaUsuário = Console.ReadLine();
+int.TryParse(entradaUsuário, out primeiroNúmero);
+
+Console.Write("Por favor insira o segundo número: ");
+string entradaUsuário2 = Console.ReadLine();
+int.TryParse(entradaUsuário2, out segundoNúmero);
+
+Console.WriteLine("O que você quer fazer?\n[A]dicionar os números\n[S]ubtrair os números\n[M]ultiplicar os números");
+string respostaUsuário = Console.ReadLine();
+
+if (respostaUsuário == "A" || respostaUsuário == "a")
+{
+    Console.WriteLine($"{primeiroNúmero} + {segundoNúmero} = {primeiroNúmero + segundoNúmero}");
+}
+else if (respostaUsuário == "S" || respostaUsuário == "s")
+{
+    Console.WriteLine($"{primeiroNúmero} - {segundoNúmero} = {primeiroNúmero - segundoNúmero}");
+}
+else if (respostaUsuário == "M" || respostaUsuário == "m")
+{
+    Console.WriteLine($"{primeiroNúmero} * {segundoNúmero} = {primeiroNúmero * segundoNúmero}");
+}
+else
+{
+    Console.WriteLine("Escolha Inválida!");
+}
+
+Console.WriteLine("Pressione qualquer tecla para fechar.");
+Console.ReadKey();
---
+++ Calculator-Calculadora/Calculator-Calculadora/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+int firstNumber; int secondNumber;
+
+Console.WriteLine("Hello!");
+Console.Write("Please input the first number: ");
+string userInputFirstNumber = Console.ReadLine();
+int.TryParse(userInputFirstNumber, out firstNumber);
+
+Console.Write("Please input the secound number: ");
+string userInputSecoundNumber = Console.ReadLine();
+int.TryParse(userInputSecoundNumber, out secondNumber);
+
+Console.WriteLine("What do want to do?");
+Console.WriteLine("[A]dd numbers");
+Console.WriteLine("[S]ubtract numbers");
+Console.WriteLine("[M]ultiply numbers");
+
+string userInputAnswer = Console.ReadLine();
+
+if (userInputAnswer == "A" || userInputAnswer == "a")
+{
+    Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+}
+else if (userInputAnswer == "S" || userInputAnswer == "s")
+{
+    Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+}
+else if (userInputAnswer == "M" || userInputAnswer == "m")
+{
+    Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+}
+else
+{
+    Console.WriteLine("Invalid choice!");
+}
+
+Console.WriteLine("Press any key to close.");
+
+Console.ReadKey();
