4f11da80d795f244d39183ceb3881a7d4b2ca4fe
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-01-14 14:16:45 -0300

    Add estruturas em Csharp

---
+++ Estruturas-Structures/Vetores-Arrays/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int[] numbers = new int[] { 2, 6, 1, 6, 19 };
+
+Console.WriteLine($"element at index 0 is {numbers[0]}");
+Console.WriteLine($"element at index 1 is {numbers[1]}");
+Console.WriteLine($"element at index 2 is {numbers[2]}");
+Console.WriteLine($"element at index 3 is {numbers[3]}");
+Console.WriteLine($"element at index 4 is {numbers[4]}");
+
+Console.WriteLine();
+
+var sum = 0;
+for (var i = 0; i < numbers.Length; ++i)
+{
+    sum += numbers[i];
+}
+Console.WriteLine($"sum of elements is {sum}");
+
+Console.WriteLine();
+
+var firstFromEnd = numbers[^1];
+var secondFromEnd = numbers[^2];
+
+Console.WriteLine($"the last one element is {firstFromEnd}");
+Console.WriteLine($"the penultimate element is {secondFromEnd}");
+
+//int[] numbers = new int[3];
+
+//Console.WriteLine($"Element at index 0 is {numbers[0]}");
+//Console.WriteLine($"Element at index 1 is {numbers[1]}");
+//Console.WriteLine($"Element at index 2 is {numbers[2]}");
+
+//Console.WriteLine();
+
+//numbers[0] = 5;
+//numbers[1] = 6;
+//numbers[2] = 7;
+
+//Console.WriteLine($"Element at index 0 is {numbers[0]}");
+//Console.WriteLine($"Element at index 1 is {numbers[1]}");
+//Console.WriteLine($"Element at index 2 is {numbers[2]}");
+
+Console.ReadKey();
---
+++ Estruturas-Structures/Vetores-Arrays/Vetores/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int[] numeros = new int[] { 2, 6, 1, 6, 19 };
+
+Console.WriteLine($"O elemento no índice 0 é {numeros[0]}");
+Console.WriteLine($"O elemento no índice 1 é {numeros[1]}");
+Console.WriteLine($"O elemento no índice 2 é {numeros[2]}");
+Console.WriteLine($"O elemento no índice 3 é {numeros[3]}");
+Console.WriteLine($"O elemento no índice 4 é {numeros[4]}");
+
+Console.WriteLine();
+
+var soma = 0;
+for (var i = 0; i < numeros.Length; ++i)
+{
+    soma += numeros[i];
+}
+Console.WriteLine($"A soma dos elementos é {soma}");
+
+Console.WriteLine();
+
+var ultimoElemento = numeros[^1];
+var penultimoElemento = numeros[^2];
+
+Console.WriteLine($"O último elemento é {ultimoElemento}");
+Console.WriteLine($"O penúltimo elemento é {penultimoElemento}");
+
+//int[] numeros = new int[3];
+
+//Console.WriteLine($"Elemento no índice 0 é {numeros[0]}");
+//Console.WriteLine($"Elemento no índice 1 é {numeros[1]}");
+//Console.WriteLine($"Elemento no índice 2 é {numeros[2]}");
+
+//Console.WriteLine();
+
+//numeros[0] = 5;
+//numeros[1] = 6;
+//numeros[2] = 7;
+
+//Console.WriteLine($"Elemento no índice 0 é {numeros[0]}");
+//Console.WriteLine($"Elemento no índice 1 é {numeros[1]}");
+//Console.WriteLine($"Elemento no índice 2 é {numeros[2]}");
+
+Console.ReadKey();
